dba885a1a3258d67a8248b8d9d3597be00e429a0
Rob <rrhamy@srclogic.com>
2024-05-08 18:15:10 -0400

    First push

--- pokereact/src/App.js
+++ pokereact/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import logo from './PokeReact.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main Page">
+      <div className="topbar p-1 flex justify-content-between flex-row align-items-center" style={{'border-bottom-style':'solid'}}> 
+        <div className="ml-3 flex">
+          <span><img src={logo} alt=""></img></span>
+        </div>
+      </div>
     </div>
   );
 }
